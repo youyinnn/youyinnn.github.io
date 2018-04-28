04bbe63b5e6076e0c13cbe460aa6d07ebedb1fb7
youyinnn <454947049@qq.com>
2018-04-28 10:50:07 +0800

    IMPMODU

--- myjs/githubapi.js
+++ myjs/githubapi.js
@@ -4,10 +4,9 @@ var post_label = 'ypost'
 var about_label = 'yabout'
 var friend_link_label = 'yfirendlink'
 var script_label = 'yscript'
-var auth_token
 var url = 'https://api.github.com'
 var client_id = '890ef13704c744878bac'
-var redirect_uri = 'https://youyinnn.github.io/'
+var client_secret = '1144f5bf192b5100f7018b15cdb8e2dabcc3467b'
 var all
 
 function getset(url) {
@@ -18,6 +17,7 @@ function getset(url) {
     'url': url,
     'headers': {
       'Authorization': 'Bearer ' + auth_token,
+      'Upgrade-Insecure-Requests': 1
     },
     'processData': false,
     'contentType': false,
@@ -29,9 +29,11 @@ function postset(url, form) {
   let basepostset = {
     'async': true,
     'crossDomain': true,
-    'method': 'GET',
+    'method': 'POST',
+    'url': url,
     'headers': {
-      'Authorization': 'Bearer ' + auth_token,
+      // 'Authorization': 'Bearer ' + auth_token,
+      'Cookie':'_ga=GA1.2.421864759.1520090149; _octo=GH1.1.612402754.1520090149; tz=Asia%2FShanghai; user_session=YoZCN69eAa7qv-K_C5-qu6t-jkk8B1Ml9w8gj9LG6cenDfQr; logged_in=yes; dotcom_user=youyinnn; _gid=GA1.2.683521117.1524844524; _gat=1; _gh_sess=KzlaRytvREdIUktPcXBjTmQ5TkVXbHczU0NGQTQ3NFIzOFRxbWcyLzFQeFVBdWFURHg1a2RlaVkvNTAwdko5TWtMVlREaWE4b1VLeTFZRnZadGZmSGhLTE5UWlNmM1pGSmdCcDdQSHMwRndvVW1MWGVjNjNxVFc5RTJSQzYxWDczcnJFMGU2bm1laWJnQkpqZ3BuZlhtckszekdVc3Z5dmRvNWd4VmlTMnFpRzBxK1Y5djJiaWUwNmQybExydElSZGpGdU1xdllZamxzMzArd0F5bm0yZ3lVdEJLWVR6UXdDbHUxbEhBOFg3V1hsUE85WWgyb3NXY2gyRFhlam01OXhRTGpSVmVLdjc0WS9nTC9kMzBZcitXMmJ4Q2xzN1FpN3FLSXA0aG5LNjgvMlVjSEk5aDFkaUZhdXowM1h3MkY0dEpqc2ttbWF4ODR3dlF4ek1uWGZaakhscURwYjZveFFtNGhmelM1cW5XcHU0S1JvUjRuVkw5cjdHcGRJWWw3dU9ncGtpS0svaVk2ems4d2FQalR6L2xYRnVoNENrUHFlYm5WME4vS0YwMmJJWURFekY1MkJuUGtzcTdUQzVENUo2aEhSQUl5YVJCS1lZaStGS3JHWHA0Ky9ZME4vSS9YbWpEa0xCdVkyeDVtdFVBRDdndG5WZmxXdnVuajRWVjJhdFJQWFJLRE85UTdiSXZXcTFrU1FVVFZoZ1Zlb0xQRVlmUkxCT1JkcVNDanRhUzF1VVltMzNrQ2l6MHp5VTFqLS0xaFdCNXFYNHRic1hXQUVNdktkNTlnPT0%3D--dabaa6d6458ac4e461568608f3fb7fecb3fbec26'
     },
     'mimeType': 'multipart/form-data',
     'data': form,
@@ -83,9 +85,4 @@ function get_posts() {
 function get_post(number, func) {
   let url = window.url + '/repos/' + user + '/' + blog_repo + '/issues/' + number
   sendget(url, func)
-}
-
-function oauth_app() {
-  let url = 'https://github.com/login/oauth/authorize?client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=public_repo'
-  sendget(url)
 }
--- myjs/init.js
+++ myjs/init.js
@@ -2,5 +2,4 @@ var topbar = $('#topbar')[0]
 
 $(function () {
   console.log(location.href)
-  oauth_app()
 })
