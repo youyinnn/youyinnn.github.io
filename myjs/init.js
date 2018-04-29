51ce3bbf521b6713674ba71182a9e63d13aeb685
youyinnn <454947049@qq.com>
2018-04-29 14:09:59 +0800

    IMPMODU

--- myjs/init.js
+++ myjs/init.js
@@ -1,9 +1,3 @@
-var pageload
-var nowerpage = 1
-var totalpages
-var postpanelheight
-var postheight
-
 $(function () {
   let search = location.search
   if (search === '') {
@@ -43,31 +37,3 @@ $(function () {
   }
 })
 
-function render_md(text) {
-  text = text.replace(/"/g, '\\"')
-  text = text.replace(/'/g, '\'')
-  editormd.markdownToHTML("md", {
-    markdown: text,
-    htmlDecode: "style,script,iframe|on",
-    tocm: true, // Using [TOCM]
-    tocContainer: "#sidetoc",
-    taskList: true,
-    readOnly: true,
-    codeFold: true,
-  });
-  let as = $('#md a')
-  for(let i = 0; i < as.length; i++) {
-    as[i].target = '_blank'
-  }
-}
-
-function postspage(pageto) {
-  $('#docpanel')[0].style.cssText = 'transform: translateY(-' + ((postpanelheight - 48) * (pageto - 1)) + 'px);'
-}
-
-function pagehandler(item, box, itemslength) {
-  postheight = item.clientHeight + 48
-  postpanelheight = box.clientHeight
-  pageload = parseInt(postpanelheight / (postheight))
-  totalpages = Math.round(itemslength / pageload)
-}
