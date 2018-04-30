069e8ea2926b72912211018ae7be7df4bcf6a2ef
youyinnn <454947049@qq.com>
2018-04-30 09:58:03 +0800

    IMPMODU

--- myjs/init.js
+++ myjs/init.js
@@ -24,6 +24,31 @@ $(function () {
       get_posts()
     } else if (key === 'panel' && value === 'post') {
       get_post(params[1].split('=')[1], function (re) {
+        let title = re.title
+        let post = document.createElement('div')
+        let posttitle = document.createElement('div')
+        let posttime = document.createElement('div')
+        let sp1 = document.createElement('span')
+        let sp2 = document.createElement('span')
+        let sp3 = document.createElement('span')
+        let sp4 = document.createElement('span')
+        addClass(post, 'post onepost')
+        addClass(posttitle, 'posttitle')
+        addClass(posttime, 'posttime')
+        addClass(sp1, 'font-weight-bold')
+        addClass(sp3, 'font-weight-bold')
+        sp1.innerHTML = 'PostTime:'
+        sp2.innerHTML = re.created_at
+        sp3.innerHTML = 'LastModTime:'
+        sp4.innerHTML = re.updated_at
+        posttitle.innerHTML = title
+        appendC(posttime, sp1)
+        appendC(posttime, sp2)
+        appendC(posttime, sp3)
+        appendC(posttime, sp4)
+        appendC(post, posttitle)
+        appendC(post, posttime)
+        appendC($('#md')[0], post)
         let text = re.body
         render_md(text)
       })
