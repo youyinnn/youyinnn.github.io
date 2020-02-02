282ae6daec0d2dcefd411e4ea131f2caffef649a
youyinnn <youyinnn@gmail.com>
2020-02-02 20:53:02 +0800

    fix grammar

--- myjs/elementcreate.js
+++ myjs/elementcreate.js
@@ -1,33 +1,33 @@
-function createpostcard(item, pagebelong) {
-    let postcard = c('div')
-    let posttitle = c('a')
-    let posttime = c('div')
+function createarticlecard(item, pagebelong) {
+    let articlecard = c('div')
+    let articletitle = c('a')
+    let articletime = c('div')
     let sp1 = c('span')
-    let postshortmsg = c('div')
-    let postmore = c('a')
+    let articleshortmsg = c('div')
+    let articlemore = c('a')
     let tagsandcates = c('div')
     let tagsbox = c('div')
     let charsbox = c('div')
     let catestag = c('span')
-    adclass(postcard, 'postcard')
-    adclass(posttitle, 'postcardtitle font-weight-bold')
-    adclass(posttime, 'postcardtime')
-    adclass(tagsandcates, 'postcardtime')
-    adclass(postshortmsg, 'postshortmsg')
-    adclass(postmore, 'postmore')
+    adclass(articlecard, 'articlecard')
+    adclass(articletitle, 'articlecardtitle font-weight-bold')
+    adclass(articletime, 'articlecardtime')
+    adclass(tagsandcates, 'articlecardtime')
+    adclass(articleshortmsg, 'articleshortmsg')
+    adclass(articlemore, 'articlemore')
     tagsbox.style.marginTop = '5px'
     charsbox.style.marginTop = '5px'
-    posttitle.innerHTML = item.title
-    posttitle.number = item.number
-    posttitle.href = '/' + '?to=post&number=' + posttitle.number
-    postcard.id = 'post_' + item.number
-    postshortmsg.id = 'post_short_msg_' + item.number
-    postmore.innerHTML = 'more'
-    postmore.href = '/' + '?to=post&number=' + posttitle.number
-    $(posttitle).click(function () {
-        location = '/' + '?to=post&number=' + posttitle.number
+    articletitle.innerHTML = item.title
+    articletitle.number = item.number
+    articletitle.href = '/' + '?to=article&number=' + articletitle.number
+    articlecard.id = 'article_' + item.number
+    articleshortmsg.id = 'article_short_msg_' + item.number
+    articlemore.innerHTML = 'more'
+    articlemore.href = '/' + '?to=article&number=' + articletitle.number
+    $(articletitle).click(function () {
+        location = '/' + '?to=article&number=' + articletitle.number
     })
-    sp1.innerHTML = '# 创于 ' + postcarddate(dayjs(item.date)) + ' | 发于 ' + postcarddate(dayjs(item.created_at)) + ' | 更于' + postcarddate(dayjs(item.updated_at))
+    sp1.innerHTML = '# 创于 ' + articlecarddate(dayjs(item.date)) + ' | 发于 ' + articlecarddate(dayjs(item.created_at)) + ' | 更于' + articlecarddate(dayjs(item.updated_at))
     let catestaghtml = ''
     let content = ''
     if (item.categories !== undefined && item.categories.length !== 0) {
@@ -39,13 +39,13 @@ function createpostcard(item, pagebelong) {
     } else {
         content = 'nothing here'
     }
-    catestaghtml = 'Categories: <span class="bdgonpostcard badge badge-dark" style="font-size: 100%;max-width: 100%;border-radius: .25rem !important;">' + content + '</span>'
+    catestaghtml = 'Categories: <span class="bdgonarticlecard badge badge-dark" style="font-size: 100%;max-width: 100%;border-radius: .25rem !important;">' + content + '</span>'
     catestag.innerHTML = catestaghtml
     let tagsboxhtml = ''
     content = ''
     if (item.tags !== undefined && item.tags.length !== 0) {
         for (let i = 0; i < item.tags.length; i++) {
-            content += '<span class="bdgonpostcard badge badge-dark" style="font-size: 100%;max-width: 100%;border-radius: .25rem !important;">' + item.tags[i] + '</span>&nbsp;'
+            content += '<span class="bdgonarticlecard badge badge-dark" style="font-size: 100%;max-width: 100%;border-radius: .25rem !important;">' + item.tags[i] + '</span>&nbsp;'
         }
     } else {
         content = 'nothing here'
@@ -53,16 +53,16 @@ function createpostcard(item, pagebelong) {
     tagsboxhtml = 'Tags: ' + content
     tagsbox.innerHTML = tagsboxhtml
     charsbox.innerHTML = 'Chars: ' + item.char_count + ' c'
-    appendc(postcard, posttitle)
-    appendc(posttime, sp1)
-    appendc(postcard, posttime)
+    appendc(articlecard, articletitle)
+    appendc(articletime, sp1)
+    appendc(articlecard, articletime)
     appendc(tagsandcates, catestag)
     appendc(tagsandcates, tagsbox)
     appendc(tagsandcates, charsbox)
-    appendc(postcard, tagsandcates)
-    appendc(postcard, postshortmsg)
-    appendc(postcard, postmore)
-    appendc($('#pagebox-' + pagebelong)[0], postcard)
+    appendc(articlecard, tagsandcates)
+    appendc(articlecard, articleshortmsg)
+    appendc(articlecard, articlemore)
+    appendc($('#pagebox-' + pagebelong)[0], articlecard)
     let text = item.short_contant
     let cq = text.match(/{%.*cq.*%}/gm)
     let emojis = text.match(/:[A-z]+[-|_]?[A-z|0-9]+:/g)
@@ -101,7 +101,7 @@ function createpostcard(item, pagebelong) {
             text = text.replace(ktmd, kthtml)
         })
     }
-    editormd.markdownToHTML(postshortmsg.id, {
+    editormd.markdownToHTML(articleshortmsg.id, {
         markdown: text,
         htmlDecode: 'style,script,iframe',
         tocm: true, // Using [TOCM]
@@ -119,39 +119,39 @@ function createpostcard(item, pagebelong) {
     }
 }
 
-function createposthead(re) {
+function createarticlehead(re) {
     let title = re.title
-    let posthead = c('div')
-    let posttitle = c('div')
-    let posttime = c('div')
+    let articlehead = c('div')
+    let articletitle = c('div')
+    let articletime = c('div')
     let sp1 = c('span')
     let sp2 = c('span')
     let sp3 = c('span')
     let sp4 = c('span')
     let sp5 = c('span')
     let sp6 = c('span')
-    adclass(posthead, 'posthead onepost')
-    adclass(posttitle, 'posttitle')
-    adclass(posttime, 'posttime')
+    adclass(articlehead, 'articlehead onearticle')
+    adclass(articletitle, 'articletitle')
+    adclass(articletime, 'articletime')
     adclass(sp1, 'font-weight-bold mr-2')
     adclass(sp3, 'font-weight-bold mr-2')
     adclass(sp5, 'font-weight-bold mr-2')
     adclass(sp6, 'mdcharlength')
     sp1.innerHTML = 'Post:'
-    sp2.innerHTML = dayjs(re.created_at).format('MMM,DD YYYY') + postcarddate(dayjs(re.created_at))
+    sp2.innerHTML = dayjs(re.created_at).format('MMM,DD YYYY') + articlecarddate(dayjs(re.created_at))
     sp3.innerHTML = '<br>Mod:'
-    sp4.innerHTML = dayjs(re.updated_at).format('MMM,DD YYYY') + postcarddate(dayjs(re.updated_at))
+    sp4.innerHTML = dayjs(re.updated_at).format('MMM,DD YYYY') + articlecarddate(dayjs(re.updated_at))
     sp5.innerHTML = '<br>Chars:'
-    posttitle.innerHTML = title
-    appendc(posttime, sp1)
-    appendc(posttime, sp2)
-    appendc(posttime, sp3)
-    appendc(posttime, sp4)
-    appendc(posttime, sp5)
-    appendc(posttime, sp6)
-    appendc(posthead, posttitle)
-    appendc(posthead, posttime)
-    appendc(md, posthead)
+    articletitle.innerHTML = title
+    appendc(articletime, sp1)
+    appendc(articletime, sp2)
+    appendc(articletime, sp3)
+    appendc(articletime, sp4)
+    appendc(articletime, sp5)
+    appendc(articletime, sp6)
+    appendc(articlehead, articletitle)
+    appendc(articlehead, articletime)
+    appendc(md, articlehead)
 }
 
 function showhexometadata(hexometadata) {
@@ -168,10 +168,10 @@ function showhexometadata(hexometadata) {
         metadatapanelbody.innerHTML += '<span class="badge badge-light">Title:</span> ' + hexometadata.title + '<br>'
     }
     if (hexometadata.date !== undefined) {
-        metadatapanelbody.innerHTML += '<span class="badge badge-light">Date:</span> ' + dayjs(hexometadata.date).set('hour', dayjs(hexometadata.date).hour() - 8).format('MMM,DD YYYY') + postcarddate(dayjs(hexometadata.date).set('hour', dayjs(hexometadata.date).hour() - 8)) + '<br>'
+        metadatapanelbody.innerHTML += '<span class="badge badge-light">Date:</span> ' + dayjs(hexometadata.date).set('hour', dayjs(hexometadata.date).hour() - 8).format('MMM,DD YYYY') + articlecarddate(dayjs(hexometadata.date).set('hour', dayjs(hexometadata.date).hour() - 8)) + '<br>'
     }
     if (hexometadata.updated !== undefined) {
-        metadatapanelbody.innerHTML += '<span class="badge badge-light">Updated:</span> ' + dayjs(hexometadata.updated).set('hour', dayjs(hexometadata.updated).hour() - 8).format('MMM,DD YYYY') + postcarddate(dayjs(hexometadata.updated).set('hour', dayjs(hexometadata.updated).hour() - 8)) + '<br>'
+        metadatapanelbody.innerHTML += '<span class="badge badge-light">Updated:</span> ' + dayjs(hexometadata.updated).set('hour', dayjs(hexometadata.updated).hour() - 8).format('MMM,DD YYYY') + articlecarddate(dayjs(hexometadata.updated).set('hour', dayjs(hexometadata.updated).hour() - 8)) + '<br>'
     }
     if (hexometadata.comments !== undefined) {
         metadatapanelbody.innerHTML += '<span class="badge badge-light">Comments:</span> ' + hexometadata.comments + '<br>'
@@ -200,7 +200,7 @@ function createtodo(issuesbody, re) {
     for (let i = re.length - 1; i >= 0; --i) {
         let head = re[i].created_at
         let text = re[i].body
-        fulltext += '<div class="card todo-card" id="search-' + (re.length - i - 1) + '"> <div class="card-header todo-head"> <span style="font-weight:bold;">ToDo <a href=" ' + re[i].html_url + '" target="_blank">#' + (re.length - i - 1) + '</a> created at: </span> <p style="margin:0; text-align: right">' + dayjs(head).format('MMM,DD YYYY') + postcarddate(dayjs(head).set('hour', dayjs(head).hour() - 8)) + ' </p> </div> <div class="card-body todo-body"> <p class="card-text todo-text">\r\n' + text + '\r\n</p> </div> </div><br>'
+        fulltext += '<div class="card todo-card" id="search-' + (re.length - i - 1) + '"> <div class="card-header todo-head"> <span style="font-weight:bold;">ToDo <a href=" ' + re[i].html_url + '" target="_blank">#' + (re.length - i - 1) + '</a> created at: </span> <p style="margin:0; text-align: right">' + dayjs(head).format('MMM,DD YYYY') + articlecarddate(dayjs(head).set('hour', dayjs(head).hour() - 8)) + ' </p> </div> <div class="card-body todo-body"> <p class="card-text todo-text">\r\n' + text + '\r\n</p> </div> </div><br>'
     }
     $(md).animateCss('fadeIn')
     searchshowandrendermd(fulltext, re.length)
@@ -215,7 +215,7 @@ function createscript(issuesbody, re) {
     for (let i = 0; i < re.length; ++i) {
         let head = re[i].created_at
         let text = re[i].body
-        fulltext += `<div class="card" id="search-${i}"><div class="card-header"><span style="font-weight:bold;">Script <a href="${re[i].html_url}" target="_blank">#${i}</a> created at: </span>${dayjs(head).format('MMM,DD YYYY') + postcarddate(dayjs(head).set('hour', dayjs(head).hour() - 8))}</div><div class="card-body">${'\r\n\r\n' + text + '\r\n\r\n'}</div></div><br>`
+        fulltext += `<div class="card" id="search-${i}"><div class="card-header"><span style="font-weight:bold;">Script <a href="${re[i].html_url}" target="_blank">#${i}</a> created at: </span>${dayjs(head).format('MMM,DD YYYY') + articlecarddate(dayjs(head).set('hour', dayjs(head).hour() - 8))}</div><div class="card-body">${'\r\n\r\n' + text + '\r\n\r\n'}</div></div><br>`
     }
     $(md).animateCss('fadeIn')
     searchshowandrendermd(fulltext, re.length)
@@ -245,13 +245,13 @@ function searchshowandrendermd(fulltext, relength) {
     render_md(fulltext)
 }
 
-function createpostcomment(i, comment) {
+function createarticlecomment(i, comment) {
     let commentCard = '<div class="comment" id="comment-' + i + '"><div class="card-header text-white bg-dark" style="border-radius: .25rem .25rem 0 0;"><span style="font-weight:bold;"><a target="_blank" href=" ' + comment.user.html_url + ' ">' + comment.user.login + '</a> commented <a href=" ' + comment.html_url + '" target="_blank">#' + i + '</a> @ : </span>' + dayjs(comment.created_at).format('MMM,DD YYYY') + ' ' + (comment.author_association === 'NONE' ? '' : (' <span class="align-text-top badge badge-light ml-1">' + comment.author_association + '</span>')) + ' </div><div class="card-body"><p class="card-text">' + (comment.body) + '</p></div></div><br>'
     return commentCard
 }
 
 function createsharebtn(btinnerHTML, func) {
-    let ps = $('#postshare')[0]
+    let ps = $('#articleshare')[0]
     let btn = c('button')
     adclass(btn, 'btn btn-dark ml-1')
     btn.innerHTML = btinnerHTML
