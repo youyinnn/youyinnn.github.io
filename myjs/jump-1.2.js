9f2fec3d9485f7b858a27eb56e2983da846aff98
youyinnn <youyinnn@gmail.com>
2020-03-13 11:04:23 +0800

    use baidumap api to check ip 2

---
+++ myjs/jump-1.2.js
@@ -0,0 +1,24 @@
+function cb(rs) {
+    if (rs.status === 0) {
+        // chinese visitor
+        sessionStorage.setItem('__ischinese', 'yes')
+        window.location.href = 'https://youyinnn.gitee.io' + location.pathname + '?fromgithub=true'
+    } else {
+        // foreign visitor
+        sessionStorage.setItem('__isforeigner', 'yes')
+    }
+}
+
+if (sessionStorage.getItem('__ischinese') === 'yes') {
+    // chinese user should always jump to gitee
+    window.location.href = 'https://youyinnn.gitee.io' + location.pathname + '?fromgithub=true'
+} else if (
+    // only in visiting github pages
+    location.origin === 'https://youyinnn.github.io' &&
+    // and non foreign user
+    sessionStorage.getItem('__isforeigner') !== 'yes') {
+
+    load('https://api.map.baidu.com/location/ip?ak=GDuCkcOTursQT2efQrpvw2g3ufIGz7rK&callback=cb', {
+        async: true
+    })
+}
