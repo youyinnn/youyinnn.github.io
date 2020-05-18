39456761c93ab7860a85342674bb3f49bd3fa8e3
youyinnn <youyinnn@gmail.com>
2020-05-18 14:10:37 +0800

    algolia racords extract strategy update

--- plugins/wordsegmentation.js
+++ plugins/wordsegmentation.js
@@ -1,5 +1,5 @@
-const nodejieba = require("nodejieba");
-
+const nodejieba = require("nodejieba")
+const fs = require('fs')
 const path = require('path')
 
 let extradictpath = path.join(__dirname, 'dict')
@@ -12,15 +12,40 @@ nodejieba.load({
     userDict: path.join(extradictpath, 'juejin-tags.txt')
 })
 
+
+const cnign = fs.readFileSync(path.join(extradictpath, 'cn.ign'), {encoding: 'utf-8'})
+const enign = fs.readFileSync(path.join(extradictpath, 'en.ign'), {encoding: 'utf-8'})
+const cnignSet = new Set(cnign.split('\n'))
+const enignSet = new Set(enign.split('\n'))
+
+function handleRaw(raw) {
+    return raw.replace(/([-:\r\n#。，,？?\/\\*<>《》()（）\[\]：‘’“”''"+=`·~!！@$￥%\^……&{}|;\s、【】——])/gm, ' ')
+}
+
 function rawStringToKeywords(raw) {
-    raw = raw.replace(/([-:\r\n#。，,？?\/\\*<>《》()（）\[\]：‘’“”''"+=`·~!！@$￥%\^……&{}|;\s、【】——])/gm, ' ')
+    raw = handleRaw(raw)
     // let rs = nodejieba.cutForSearch(raw, false)
-    let rs = nodejieba.extract(raw, 300)
+    let rs = nodejieba.extract(raw, 500)
     let newrs = []
     for (rss of rs) {
-        newrs.push(rss.word)
+        if (!cnignSet.has(rss.word) && !enignSet.has(rss.word.toLowerCase())) {
+            newrs.push(rss.word)
+        }
     }
     return newrs
 }
 
+function rawStringToKeywordsForSearch(raw) {
+    raw = handleRaw(raw)
+    let rs = nodejieba.cut(raw, false)
+    let newrs = new Set()
+    for (rss of rs) {
+        if (!cnignSet.has(rss) && !enignSet.has(rss.toLowerCase())) {
+            newrs.add(rss)
+        }
+    }
+    return Array.from(newrs)
+}
+
 module.exports.rawStringToKeywords = rawStringToKeywords
+module.exports.rawStringToKeywordsForSearch = rawStringToKeywordsForSearch
