31b937008dd3c7091aa73874d1fd1b3415aaef0b
youyinnn <youyinnn@gmail.com>
2020-10-25 23:02:11 +0800

    commit from yexo at Sun Oct 25 2020 23:02:11 GMT+0800 (中国标准时间)

--- resources/resources.js
+++ resources/resources.js
@@ -1,1 +1,1 @@
-sessionStorage.setItem('cacheversion', 1603620477553); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-gocpd5.js"];} else {resourcesList = ["friendslink.js"];}
+sessionStorage.setItem('cacheversion', 1603638127786); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-n5ubg8.js"];} else {resourcesList = ["friendslink.js"];}
