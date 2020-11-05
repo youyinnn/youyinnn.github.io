7d986e8e4a8d7833c9018211f7a6a81a52f13d5d
youyinnn <youyinnn@gmail.com>
2020-11-05 19:59:26 +0800

    commit from yexo at Thu Nov 05 2020 19:59:26 GMT+0800 (中国标准时间)

--- resources/resources.js
+++ resources/resources.js
@@ -1,1 +1,1 @@
-sessionStorage.setItem('cacheversion', 1603638127786); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-n5ubg8.js"];} else {resourcesList = ["friendslink.js"];}
+sessionStorage.setItem('cacheversion', 1604577542169); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-m4kcnt.js"];} else {resourcesList = ["friendslink.js"];}
