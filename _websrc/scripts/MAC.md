653700b16b90fc3c73bcb83a4054624caed69912
youyinnn <youyinnn@gmail.com>
2021-04-19 21:54:18 +0800

    commit from yexo at Mon Apr 19 2021 21:54:18 GMT+0800 (中国标准时间)

---
+++ _websrc/scripts/MAC.md
@@ -0,0 +1,12 @@
+## MAC
+
+### MAC M1系统怎么进入恢复模式
+
+Intel的MAC是在开机显示logo的时候按下`command + R`，而M1的MAC只需要长按开机键就能进入了
+
+
+
+### MAC启动安全性实用工具
+
+https://support.apple.com/zh-cn/HT208198
+
--- resources/resources.js
+++ resources/resources.js
@@ -1,1 +1,1 @@
-sessionStorage.setItem('cacheversion', 1611904406609); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-16t9xb1.js"];} else {resourcesList = ["friendslink.js"];}
+sessionStorage.setItem('cacheversion', 1618840455180); var articlesCount = 120; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-1h43jl0.js"];} else {resourcesList = ["friendslink.js"];}
