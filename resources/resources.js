ba84a5ba2991cf1930804781b88570f83cb1bacc
youyinnn <youyinnn@gmail.com>
2021-11-22 20:01:38 -0500

    commit from yexo at Mon Nov 22 2021 20:01:38 GMT-0500 (Eastern Standard Time)

--- resources/resources.js
+++ resources/resources.js
@@ -1,1 +1,1 @@
-sessionStorage.setItem('cacheversion', 1637108643485); var articlesCount = 121; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-1udmggk.js"];} else {resourcesList = ["friendslink.js"];}
+sessionStorage.setItem('cacheversion', 1637629291749); var articlesCount = 121; var resourcesList;if (location.pathname.startsWith('/articles/') || location.pathname.startsWith('/article/')) {resourcesList = ["friendslink.js","cache-yxjr49.js"];} else {resourcesList = ["friendslink.js"];}
