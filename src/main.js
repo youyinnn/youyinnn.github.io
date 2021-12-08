038bc81744afd50a4ed54e9ec1316812e7dafd12
youyinnn <youyinnn@gmail.com>
2021-12-08 12:35:46 -0500

    stage 3: article metadata

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "vfonts/Lato.css";
+import "./assets/css/index.less";
+import "animate.css";
+
 createApp(App).use(router).use(store).mount("#app");
