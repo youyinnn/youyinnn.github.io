1aa6b48b39c7ada5529d42068fb38f6653e79125
youyinnn <youyinnn@gmail.com>
2021-12-07 21:59:05 -0500

    css

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,59 @@
 <template>
-  <TopBar />
-  <div class="main">
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
-  </div>
+  <n-config-provider :theme="theme">
+    <TopBar />
+    <n-card class="main" :bordered="false" content-style="padding: 0;">
+      <router-view v-slot="{ Component }">
+        <transition name="fade3" mode="out-in">
+          <component class="page" :is="Component" />
+        </transition>
+      </router-view>
+    </n-card>
+  </n-config-provider>
 </template>
 
 <script>
 import TopBar from "@/components/TopBar.vue";
+import { defineComponent, ref } from "vue";
+import { darkTheme, NConfigProvider, NCard } from "naive-ui";
 
-export default {
+export default defineComponent({
+  setup() {
+    return {
+      darkTheme,
+      theme: ref(darkTheme),
+    };
+  },
   name: "App",
 
   data: () => ({
     //
   }),
   components: {
+    NCard,
     TopBar,
+    NConfigProvider,
   },
-};
+});
 </script>
 
 <style scoped lang="less">
 @import "../src/assets/css/index.less";
 .main {
   position: absolute;
-  top: @top-bar-height;
-  bottom: 0;
+  margin: auto;
   right: 0;
   left: 0;
-  background-color: azure;
+  top: @top-bar-height;
+  bottom: 0;
+  border-radius: 0;
+}
+
+.page {
+  max-width: @page-max-width;
+  margin-top: 0.5rem !important;
+  position: absolute;
+  margin: auto;
+  right: 0;
+  left: 0;
 }
 </style>
