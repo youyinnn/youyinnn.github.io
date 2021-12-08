ddb3e21f8775fd18d33095cc448faada2eedfda8
youyinnn <youyinnn@gmail.com>
2021-12-07 21:15:38 -0500

    stage 1

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view v-slot="{ Component }">
-        <transition name="fade" mode="out-in">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </v-main>
-  </v-app>
+  <TopBar />
+  <div class="main">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script>
+import TopBar from "@/components/TopBar.vue";
+
 export default {
   name: "App",
 
   data: () => ({
     //
   }),
+  components: {
+    TopBar,
+  },
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 @import "../src/assets/css/index.less";
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
+.main {
+  position: absolute;
+  top: @top-bar-height;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: azure;
 }
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="top-bar">
+    <n-space class="tab-box" justify="center">
+      <n-tabs type="line" size="small" tab-style="tab-style">
+        <n-tab
+          v-for="tab in tabs"
+          :key="tab.name"
+          :name="tab.name"
+          @click="goTo(tab.route)"
+          >{{ tab.text }}
+        </n-tab>
+      </n-tabs>
+    </n-space>
+  </div>
+</template>
+
+<script>
+import { NTabs, NTab, NSpace } from "naive-ui";
+
+export default {
+  name: "TopBar",
+  components: {
+    NTabs,
+    NTab,
+    NSpace,
+  },
+  data: () => ({
+    tabs: [
+      {
+        name: "home",
+        text: "Home",
+        route: "/",
+      },
+      {
+        name: "script",
+        text: "Script",
+        route: "/",
+      },
+      {
+        name: "about",
+        text: "About",
+        route: "/about",
+      },
+    ],
+  }),
+  methods: {
+    goTo(url) {
+      this.$router.push(url).catch(() => {});
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "../assets/css/index.less";
+
+.top-bar {
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  height: @top-bar-height;
+  // background-color: aquamarine;
+}
+
+.tab-box {
+  margin-top: 0rem !important;
+  max-width: 800px;
+}
+</style>
+
+<style>
+.n-tabs-tab__label {
+  color: rgb(66, 69, 109) !important;
+}
+.n-tabs-bar {
+  background-color: rgb(66, 69, 109) !important;
+}
+</style>
