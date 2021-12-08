f21792bfd071cca671781204e621dd5648629a6c
youyinnn <youyinnn@gmail.com>
2021-12-07 19:13:04 -0500

    init vue setp 2;

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(vuetify).mount("#app");
