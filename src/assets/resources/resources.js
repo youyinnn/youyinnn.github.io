8f7473792783aece667909746d3552c42f412f6f
youyinnn <youyinnn@gmail.com>
2021-12-14 23:53:06 -0500

    remove folder & vue cdn to avoid component twinkle

--- src/assets/resources/resources.js
+++ src/assets/resources/resources.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
-var resourcesList = ["cache-lu421v.js","code-theme-css-ef37e5d9.js"];
+var resourcesList = ["cache-18wuwdz.js","code-theme-css-ef37e5d9.js"];
 module.exports.list = resourcesList
   
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,4 @@
 /* eslint-disable no-unused-vars */
-const externals = {
-  vue: "Vue",
-  "vue-router": "VueRouter",
-  vuex: "Vuex",
-};
-const cdn = {
-  css: [],
-  js: [
-    "https://cdn.jsdelivr.net/npm/vue@3.2.24/dist/vue.global.min.js",
-    "https://cdn.jsdelivr.net/npm/vue-router@4.0.12/dist/vue-router.global.min.js",
-    "https://cdn.jsdelivr.net/npm/vuex@4.0.2/dist/vuex.global.min.js",
-  ],
-};
 
 const CompressionPlugin = require("compression-webpack-plugin");
 
@@ -21,32 +8,21 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
   // make the index.html file place at the root of the repo
   indexPath: "../index.html",
-  pluginOptions: {
-    // webpackBundleAnalyzer: {
-    //   openAnalyzer: false,
-    // },
-  },
+  runtimeCompiler: true,
   chainWebpack: (config) => {
     config.module.rule("raw-loader").test(/\.md$/).use("raw-loader");
     // config
     //   .plugin("webpack-bundle-analyzer")
     //   .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin);
-    config.plugin("html").tap((args) => {
-      // if (process.env.NODE_ENV === "production") {
-      args[0].cdn = cdn;
-      // }
-      return args;
-    });
     // 移除 prefetch 插件
     config.plugins.delete("prefetch");
     // 移除 preload 插件
     config.plugins.delete("preload");
-    // config.resolve.alias.set("vue", "vue/dist/vue.esm-bundler.js");
   },
+
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "production") {
       return {
-        externals: externals,
         plugins: [
           new CompressionPlugin({
             test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
@@ -56,9 +32,7 @@ module.exports = {
         ],
       };
     } else {
-      return {
-        externals: externals,
-      };
+      return {};
     }
   },
 };
