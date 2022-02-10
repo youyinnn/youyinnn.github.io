7011cb7c77b5294a15619e5a6517c2c30030847e
youyinnn <youyinnn@gmail.com>
2022-02-10 00:50:40 -0500

    update

--- src/assets/resources/resources.js
+++ src/assets/resources/resources.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
-var resourcesList = ["cache-1hrvn8j.js","code-theme-css-ef37e5d9.js"];
+var resourcesList = ["cache-1bwqy11.js","code-theme-css-ef37e5d9.js"];
 module.exports.list = resourcesList
   
