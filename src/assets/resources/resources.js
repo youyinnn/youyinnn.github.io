ad2d96a7fbc0bb0da04d3a8a5c7b7c66664d3528
youyinnn <youyinnn@gmail.com>
2023-08-23 23:33:10 -0400

    local build;

--- src/assets/resources/resources.js
+++ src/assets/resources/resources.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
-var resourcesList = ["cache-lqgb27.js","code-theme-css-ef37e5d9.js"];
+var resourcesList = ["cache-29vs6i.js","code-theme-css-ef37e5d9.js"];
 module.exports.list = resourcesList
   
